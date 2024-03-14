de0b8b7c779b92d6d7e5004c33949033e5c5a851
RoxVet <rezaandika1234509876@gmail.com>
2024-03-14 10:49:46 +0700

    update

---
+++ 4-smash.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// 2/366
+// URL : https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+
+// function smash (words) {
+//     return words.join(' ');
+//  };
+
+const smash = (words) => words.join(' ');
+
+ console.log(smash(["this", "is", "a", "really", "long", "sentence"]), "this is a really long sentence");
