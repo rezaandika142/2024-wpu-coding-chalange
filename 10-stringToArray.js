5233aa491934f8669069b644d0faf834cbe467a8
RoxVet <rezaandika1234509876@gmail.com>
2024-03-25 10:59:53 +0700

    day-10/366

--- 10-stringToArray.js
+++ 10-stringToArray.js
@@ -2,6 +2,23 @@
 // 10/366
 // URL : https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
 
+// function stringToArray(string){
+//     var result = [];
+//     var traverse = function foo(string){
+//         for(var i=0;i<string.length;i++){
+//             var chr = string.charAt(i);
+//             if(chr === " "){
+//                 result.push(string.slice(0,i));
+//                 foo(string.slice(i+1));
+//                 return;
+//             }
+//         }
+//         result.push(string);
+//     };
+//     traverse(string);
+//     return result;
+// }
+
 const stringToArray = (string) => string.split(' ');
 
 console.log(stringToArray("I love arrays they are my favorite"));
