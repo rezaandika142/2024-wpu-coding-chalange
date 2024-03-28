a27199c100dc1528726b41aebe1100c253abc912
RoxVet <rezaandika1234509876@gmail.com>
2024-03-28 13:52:23 +0700

    day-13/366

---
+++ 13-points.js
@@ -0,0 +1,22 @@
+// WPU Coding Challenge 2024
+// 13/366
+// URL : https://www.codewars.com/kata/5bb904724c47249b10000131
+
+// function points(games) {
+
+//     let total = 0;
+//     for (let i = 0; i < games.length; i++) {
+//         if (games[i][0] > games[i][2]) {
+//             total += 3;
+//         } else if (games[i][0] === games[i][2]) {
+//             total += 1;
+//         }
+//     }
+//     return total
+// }
+
+
+const points = (games) => games.reduce((acc, [x, _, y]) => (x > y ? 3 : x < y ? 0 : 1) + acc, 0);
+
+
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]));
