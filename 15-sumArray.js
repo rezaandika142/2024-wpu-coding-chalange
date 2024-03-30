6c62ca825f5f67525411ee921a7ec6b52a3cb5e5
RoxVet <rezaandika1234509876@gmail.com>
2024-03-30 11:23:33 +0700

    day 15/366

---
+++ 15-sumArray.js
@@ -0,0 +1,36 @@
+// WPU Coding Challenge 2024
+// 13/366
+// URL : https://www.codewars.com/kata/576b93db1129fcf2200001e6
+
+// function sumArray(array) {
+//     if(array === null || array.length <= 2 ) return 0;
+//     const sorted = array.sort((a,b) => a - b);
+//     let result = 0;
+//     for(let i = 1; i < sorted.length -1; i++){
+//         result += sorted[i];
+//     }
+//     return result;
+// }
+
+// function sumArray(array) {
+//     if(array === null || array.length <= 2 ) return 0;
+//     return array
+//         .sort((a,b) => a - b)
+//         .slice(1, -1)
+//         .reduce((acc, curr) => acc + curr);
+// }
+
+// function sumArray(array) {
+//     return array === null || array.length <= 2
+//     ? 0
+//     : array
+//         .sort((a,b) => a - b)
+//         .slice(1, -1)
+//         .reduce((acc, curr) => acc + curr);
+// }
+
+// const sumArray = (array) => array == null || array.length <= 2 ? 0 : array .sort((a,b) => a - b).slice(1, -1).reduce((acc, curr) => acc + curr);
+
+sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
+
+console.log(sumArray([ 6, 2, 1, 8, 10 ]))
